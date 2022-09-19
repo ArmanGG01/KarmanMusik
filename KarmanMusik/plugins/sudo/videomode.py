101c6e04743cea3c3a79da43c33b3338bd292c09
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-09-20 02:42:38 +0700

    Update videomode.py

--- KarmanMusik/plugins/sudo/videomode.py
+++ KarmanMusik/plugins/sudo/videomode.py
@@ -3,10 +3,10 @@ from pyrogram.types import Message
 
 import config
 from strings import get_command
-from PrimeMusic import app
-from PrimeMusic.misc import SUDOERS
-from PrimeMusic.utils.database import add_off, add_on
-from PrimeMusic.utils.decorators.language import language
+from KarmanMusik import app
+from KarmanMusik.misc import SUDOERS
+from KarmanMusik.utils.database import add_off, add_on
+from KarmanMusik.utils.decorators.language import language
 
 # Commands
 VIDEOMODE_COMMAND = get_command("VIDEOMODE_COMMAND")
