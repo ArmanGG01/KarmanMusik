10e8c4ff65c6c0547178ff3306d9624d9cc6b765
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-09-20 02:35:48 +0700

    Update autoend.py

--- KarmanMusik/plugins/sudo/autoend.py
+++ KarmanMusik/plugins/sudo/autoend.py
@@ -2,10 +2,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from PrimeMusic import app
-from PrimeMusic.misc import SUDOERS
-from PrimeMusic.utils.database import autoend_off, autoend_on
-from PrimeMusic.utils.decorators.language import language
+from KarmanMusik import app
+from KarmanMusik.misc import SUDOERS
+from KarmanMusik.utils.database import autoend_off, autoend_on
+from KarmanMusik.utils.decorators.language import language
 
 # Commands
 AUTOEND_COMMAND = get_command("AUTOEND_COMMAND")
