e82030acb0f5fa02f684454333b34d45ff948654
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-09-20 02:09:05 +0700

    update stop.py

--- KarmanMusik/plugins/admins/stop.py
+++ KarmanMusik/plugins/admins/stop.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from PrimeMusic.utils.database import set_loop
+from KarmanMusik.utils.database import set_loop
 
 from config import BANNED_USERS
 from strings import get_command
-from PrimeMusic import app
-from PrimeMusic.core.call import Prime
-from PrimeMusic.utils.decorators import AdminRightsCheck
+from KarmanMusik import app
+from KarmanMusik.core.call import Karman
+from KarmanMusik.utils.decorators import AdminRightsCheck
 
 # Commands
 STOP_COMMAND = get_command("STOP_COMMAND")
@@ -22,7 +22,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Prime.stop_stream(chat_id)
+    await Karman.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_9"].format(message.from_user.mention)
