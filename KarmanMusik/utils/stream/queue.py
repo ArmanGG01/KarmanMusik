e66f68fdf81854c88f3cfe82c1cc82805dde016f
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-09-20 03:11:41 +0700

    Update queue.py

--- KarmanMusik/utils/stream/queue.py
+++ KarmanMusik/utils/stream/queue.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from config import autoclean, chatstats, userstats
 from config.config import time_to_seconds
-from PrimeMusic.misc import db
+from KarmanMusik.misc import db
 
 
 async def put_queue(
