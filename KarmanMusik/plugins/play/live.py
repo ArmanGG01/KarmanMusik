fdbf1f78c0fa598e510568d3cb60984ff2a137b9
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-09-20 02:30:56 +0700

    Update live.py

--- KarmanMusik/plugins/play/live.py
+++ KarmanMusik/plugins/play/live.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 
 from config import BANNED_USERS
-from PrimeMusic import YouTube, app
-from PrimeMusic.utils.channelplay import get_channeplayCB
-from PrimeMusic.utils.decorators.language import languageCB
-from PrimeMusic.utils.stream.stream import stream
+from KarmanMusik import YouTube, app
+from KarmanMusik.utils.channelplay import get_channeplayCB
+from KarmanMusik.utils.decorators.language import languageCB
+from KarmanMusik.utils.stream.stream import stream
 
 
 @app.on_callback_query(filters.regex("LiveStream") & ~BANNED_USERS)
