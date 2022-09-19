38f38fca330218db6d70da91afc321e4e0308510
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-09-20 03:11:12 +0700

    Update db_mongo.py

--- KarmanMusik/utils/database/db_mongo.py
+++ KarmanMusik/utils/database/db_mongo.py
@@ -1,6 +1,6 @@
 from typing import Dict, List, Union
 
-from PrimeMusic.core.mongo import mongodb
+from KarmanMusik.core.mongo import mongodb
 
 queriesdb = mongodb.queries
 userdb = mongodb.userstats
