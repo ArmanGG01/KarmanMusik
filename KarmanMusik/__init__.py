6c70d19cad506903cc06f274ecd2b1b383dfcccc
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-09-20 01:03:24 +0700

    Create __init__.py

---
+++ KarmanMusik/__init__.py
@@ -0,0 +1,5 @@
+from PrimeMusic.core.bot import PrimeBot
+from PrimeMusic.core.dir import dirr
+from PrimeMusic.core.git import git
+from PrimeMusic.core.userbot import Userbot
+from PrimeMusic.misc import dbb, heroku, sudo
