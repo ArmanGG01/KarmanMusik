9ed1e5e256bd83b2c4e5e2f41282c43c16ece2d7
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-09-20 02:49:50 +0700

    Update songs.py

--- KarmanMusik/plugins/tools/songs.py
+++ KarmanMusik/plugins/tools/songs.py
@@ -11,10 +11,10 @@ from pyrogram.types import (InlineKeyboardButton,
 from config import (BANNED_USERS, SONG_DOWNLOAD_DURATION,
                     SONG_DOWNLOAD_DURATION_LIMIT)
 from strings import get_command
-from PrimeMusic import YouTube, app
-from PrimeMusic.utils.decorators.language import language, languageCB
-from PrimeMusic.utils.formatters import convert_bytes
-from PrimeMusic.utils.inline.song import song_markup
+from KarmanMusik import YouTube, app
+from KarmanMusik.utils.decorators.language import language, languageCB
+from KarmanMusik.utils.formatters import convert_bytes
+from KarmanMusik.utils.inline.song import song_markup
 
 # Command
 SONG_COMMAND = get_command("SONG_COMMAND")
