fd5a93aeff129c119539534b586471eafd502230
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-09-20 02:36:50 +0700

    Update block.py

--- KarmanMusik/plugins/sudo/block.py
+++ KarmanMusik/plugins/sudo/block.py
@@ -3,10 +3,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from PrimeMusic import app
-from PrimeMusic.misc import SUDOERS
-from PrimeMusic.utils.database import add_gban_user, remove_gban_user
-from PrimeMusic.utils.decorators.language import language
+from KarmanMusik import app
+from KarmanMusik.misc import SUDOERS
+from KarmanMusik.utils.database import add_gban_user, remove_gban_user
+from KarmanMusik.utils.decorators.language import language
 
 # Command
 BLOCK_COMMAND = get_command("BLOCK_COMMAND")
