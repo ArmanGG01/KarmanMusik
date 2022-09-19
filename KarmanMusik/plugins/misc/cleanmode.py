043e78e4a1dc7df843d00f4a394450cc1dd7e78f
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-09-20 02:26:53 +0700

    Update cleanmode.py

--- KarmanMusik/plugins/misc/cleanmode.py
+++ KarmanMusik/plugins/misc/cleanmode.py
@@ -8,9 +8,9 @@ from pyrogram.raw import types
 import config
 from config import adminlist, chatstats, clean, userstats
 from strings import get_command
-from PrimeMusic import app, userbot
-from PrimeMusic.misc import SUDOERS
-from PrimeMusic.utils.database import (get_active_chats,
+from KarmanMusik import app, userbot
+from KarmanMusik.misc import SUDOERS
+from KarmanMusik.utils.database import (get_active_chats,
                                        get_authuser_names, get_client,
                                        get_particular_top,
                                        get_served_chats,
@@ -18,8 +18,8 @@ from PrimeMusic.utils.database import (get_active_chats,
                                        is_cleanmode_on, set_queries,
                                        update_particular_top,
                                        update_user_top)
-from PrimeMusic.utils.decorators.language import language
-from PrimeMusic.utils.formatters import alpha_to_int
+from KarmanMusik.utils.decorators.language import language
+from KarmanMusik.utils.formatters import alpha_to_int
 
 BROADCAST_COMMAND = get_command("BROADCAST_COMMAND")
 AUTO_DELETE = config.CLEANMODE_DELETE_MINS
