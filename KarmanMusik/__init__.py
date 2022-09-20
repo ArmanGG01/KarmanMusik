86ee131d0673093e5b9de92f53bba5b9a0755b25
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-09-20 15:16:01 +0700

    Update __init__.py

--- KarmanMusik/__init__.py
+++ KarmanMusik/__init__.py
@@ -1,13 +1,4 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
-from KarmanMusik import KarmanBot
+from KarmanMusik.core.bot import KarmanBot
 from KarmanMusik.core.dir import dirr
 from KarmanMusik.core.git import git
 from KarmanMusik.core.userbot import Userbot
