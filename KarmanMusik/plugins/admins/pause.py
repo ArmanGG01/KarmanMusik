fc4ad78e56ca0b2b3b236839fd8c66826e4e701e
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-09-20 16:09:14 +0700

    Update pause.py

--- KarmanMusik/plugins/admins/pause.py
+++ KarmanMusik/plugins/admins/pause.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from KarmanMusik import app
-from KarmanMusik.core.call import Prime
+from KarmanMusik.core.call import Karman
 from KarmanMusik.utils.database import is_music_playing, music_off
 from KarmanMusik.utils.decorators import AdminRightsCheck
 
@@ -25,7 +25,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Prime.pause_stream(chat_id)
+    await Karman.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention)
     )
