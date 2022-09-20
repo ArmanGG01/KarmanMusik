1d3bc07e7afe6d19617da42159412545218f42cb
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-09-20 16:22:49 +0700

    Update mongo.py

--- KarmanMusik/core/mongo.py
+++ KarmanMusik/core/mongo.py
@@ -11,7 +11,7 @@ TEMP_MONGODB = "mongodb+srv://shikhar:shikhar@cluster0.6xzlh.mongodb.net/myFirst
 
 if config.MONGO_DB_URI is None:
     LOGGER(__name__).warning(
-        "No MONGO DB URL found.. Your Bot will work on Prime's Database"
+        "No MONGO DB URL found.. Your Bot will work on Karman's Database"
     )
     temp_client = Client(
         "karman",
